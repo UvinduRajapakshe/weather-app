aff535f7b675fbf9ab70372db219834e573f2927
Uvindu Bro <79355885+UvinduBro@users.noreply.github.com>
2021-11-10 13:02:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,13 +25,13 @@ locationBtn.addEventListener("click", () =>{
 });
 
 function requestApi(city){
-    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=your_api_key`;
+    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=b0bdbe14096e5117fbfc05131797535e`;
     fetchData();
 }
 
 function onSuccess(position){
     const {latitude, longitude} = position.coords; // getting lat and lon of the user device from coords obj
-    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=your_api_key`;
+    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=b0bdbe14096e5117fbfc05131797535e`;
     fetchData();
 }
 
